94a4d458cf11c9ea90e24b3a2f0bd27b081af10e
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-09-13 05:48:20 +0800

    Initial Commit

---
+++ FCC/FCC.h
@@ -0,0 +1,16 @@
+#ifndef FCC_H
+#define FCC_H
+
+class MyFCC{
+private:
+    
+public:
+    MyFCC(){
+
+    }
+private:
+
+
+};
+
+#endif // FCC_H
---
+++ includes.h
@@ -0,0 +1,13 @@
+#ifndef INCLUDES_H
+#define INCLUDES_H
+
+// Streams
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+// containers
+#include <string>
+
+
+#endif // INCLUDES_H
