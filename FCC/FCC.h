e96cc9c6981710194220724a39f315f1df06dda1
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-09-13 05:59:14 +0800

    Added createFlashcard and checkAnswer in FCC class

--- FCC/FCC.h
+++ FCC/FCC.h
@@ -1,18 +1,30 @@
 #ifndef FCC_H
 #define FCC_H
 
-#include "../includes.h"
+#include "Flashcard.h"
 
 class MyFCC{
 private:
-    std::vector<std::string> _fFront, _fBack;
+    std::vector<Flashcard> _fFront, _fBack;
     bool isFront = true;
 public:
     MyFCC(){
 
     }
-private:
 
+public:
+    void InitMenu(){
+
+    }
+
+
+private:
+    void createFlashcard(std::string fData, std::string bData){
+
+    }
+    void checkAnswer(Flashcard fc, std::string data){
+        
+    }
 
 };
 
