65b6584b52a9dd9df6cd50790ff83c5e196de25e
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-09-13 08:00:59 +0800

    Added addSet method

--- FCC/FC_Folder.h
+++ FCC/FC_Folder.h
@@ -7,10 +7,12 @@ class FC_Folder{
 private:
     std::vector<FC_Set*> _sets;
 public:
+    void addSet(FC_Set *set){
+        _sets.push_back(set);
+    }
     std::vector<FC_Set*> getSet(){
         return _sets;
     }
-    
 };
 
 #endif // FC_Folder_H
