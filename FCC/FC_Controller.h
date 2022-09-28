8b99ac31379471734cb14f3c68776bbb271eb48e
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-09-28 23:23:03 +0800

    Added createColder() method

--- FCC/FC_Controller.h
+++ FCC/FC_Controller.h
@@ -31,11 +31,14 @@ public: // Initial Functions
             // Get the Input
             std::string userInput;
             std::getline(std::cin, userInput);
+            
+            // I mean, I don't need to think about security... So I'll use it
 
             if(userInput == "1"){
-
+                system("cls");
+                showFolders();
             }else if(userInput == "2"){
-
+                createFolder();
             }else if(userInput == "3"){
 
             }
@@ -46,14 +49,44 @@ public: // Initial Functions
 public: // Menu Choices
     void showFolders(){
         int size = _folders.size();
+        std::cout << std::string(20,'=') << '\n';
+        // Prints all the folder name
         for(int i = 0; i < size; ++i){
             std::cout << '[' << i << ']' << ' ' << _folders[i]->getFolderName() << '\n';
         }
+        std::cout << std::string(20,'=') << '\n';
+    }
+    void createFolder(){
+        bool isNotSatisfied = true;
+
+        while(isNotSatisfied){
+            std::cout << "Enter the folder name: ";
+            std::string userInput;
+            std::getline(std::cin, userInput);
+
+            std::cout << "Are you satisfied with \"" << userInput << "\"?\n [YES or No] >";
+
+            std::string userChoice;
+            std::getline(std::cin, userChoice);
+            if(transformToLower(userChoice) == "yes"){
+                break;   
+            }
+        }
+        system("cls");
     }
 
 public: // Factories
     void createFlashcard(std::string front, std::string back){
         _flashcardBuffer.push_back(new Flashcard(front, back));
+    }
+
+private: // Utility
+    std::string transformToLower(std::string str){
+        int size = str.length();
+        for(int i = 0; i < size; ++i){
+            str[i] = tolower(str[i]);
+        }
+        return str;
     }
 
 private:
