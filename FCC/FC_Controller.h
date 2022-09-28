36db7f2464b261c30b7b53dac2e0f7927a76ad22
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-09-28 23:00:21 +0800

    Added createFlashcard() method

--- FCC/FC_Controller.h
+++ FCC/FC_Controller.h
@@ -9,15 +9,21 @@ private:
     FC_Controller(){
 
     }
-public:
+public: // Singleton
     static FC_Controller *get_instance(){
         if(!my_instance){
             my_instance = new FC_Controller();
         }
         return my_instance;
     }
-public:
+public: // Factories
+    void createFlashcard(std::string front, std::string back){
+        _flashcardBuffer.push_back(new Flashcard(front, back));
+    }
 
+
+private:
+    std::vector<Flashcard*> _flashcardBuffer;
 };
 
 
