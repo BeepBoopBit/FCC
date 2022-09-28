a61e662a11ee6fc29bfabe87bfb7f58b31455980
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-09-28 23:12:20 +0800

    Added Getters and Setters

--- FCC/FC_Folder.h
+++ FCC/FC_Folder.h
@@ -8,9 +8,23 @@ public: // Constructors
     FC_Folder(){
 
     }
+    FC_Folder(std::string folderName){
+        _folderName = folderName;
+    }
+
+public: // Getters
+    std::string getFolderName(){
+        return _folderName;
+    }
+
+public: // Setters
+    std::string setFolderName(std::string name){
+        _folderName = name;
+    }
 
 private: // Variables
     std::vector<FC_Set*> _sets;
+    std::string _folderName;
 };
 
 #endif // FC_Folder_H
