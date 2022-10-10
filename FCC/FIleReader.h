4557b81b9556b95f76cafa12f3756a78a742f7a5
Renz Aguirre <w.cbrenzaguirrecb@gmail.com>
2022-10-10 19:17:07 +0800

    Added File Structure Comment

--- FCC/FIleReader.h
+++ FCC/FIleReader.h
@@ -2,13 +2,14 @@
 #define _FILEREADER_H
 
 #include <string>
+#include <vector>
 #include <fstream>
 
 class FileReader{
 private:
-    std::string _fileName;
-    std::string _fileContent;
-    std::ifstream _fileStream;
+    std::string                 _fileName;
+    std::vector<std::string>    _fileContent;
+    std::ifstream               _fileStream;
 public:
     FileReader(std::string fileName){
         _fileName = fileName;
@@ -16,12 +17,17 @@ public:
     ~FileReader(){
         _fileStream.close();
     }
+    /* File Structure
+     * FolderName
+     * SetName#1,SetName#2,SetName#3
+     * {SetName#1 Data#1,SetName#1 Data#2} {SetName#2 Data#1,SetName#2 Data#2} {SetName#3 Data#1,SetName#3 Data#2}
+     */
     std::string getFileContent(){
         _fileStream.open(_fileName);
         if(_fileStream.is_open()){
             std::string line;
             while(std::getline(_fileStream, line)){
-                _fileContent += line + ' ';
+                _fileContent.push_back(line + '\n');
             }
         }
     }
